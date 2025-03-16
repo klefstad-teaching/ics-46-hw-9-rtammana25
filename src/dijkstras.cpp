94963d51beed6c485cc0afd6a93c65f0470a51d6
rtammana25 <rtammana@uci.edu>
2025-03-16 02:01:18 +0000

    finished everything

---
+++ src/dijkstras.cpp
@@ -0,0 +1,60 @@
+#include "dijkstras.h"
+#include <algorithm>
+
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+    int n = G.numVertices;
+
+    vector<int> distance(n, INF);
+    previous.resize(n, -1);
+    vector<int> visited(n, false);
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, source});
+    distance[source] = 0;
+
+    while (!pq.empty()) {
+        int u = pq.top().first;
+        pq.pop();
+
+        if (visited[u])
+            continue;
+
+        visited[u] = true;
+
+        for (const Edge& edge : G[u]) {
+            int v = edge.dst;
+            int weight = edge.weight;
+
+            if (!visited[v] && distance[u] + weight < distance[v]) {
+                distance[v] = distance[u] + weight;
+                previous[v] = u;
+                pq.push({distance[v], v});
+            }
+        }
+    }
+    return distance;
+}
+
+vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
+    vector<int> shortestPath;
+
+    for (int curr = destination; curr != -1; curr = previous[curr])
+        shortestPath.push_back(curr);
+
+    std::reverse(shortestPath.begin(), shortestPath.end());
+    return shortestPath;
+}
+
+void print_path(const vector<int>& v, int total) {
+    if (v.empty()) {
+        std::cout << "No Path!" << std::endl;
+        return;
+    }
+
+    std::cout << "Path: " << std::endl;
+    for (int vertex : v)
+        std::cout << vertex << " ";
+
+    std::cout << std::endl;
+    std::cout << "Total cost is " << total << std::endl;
+}
---
+++ src/ladder.cpp
@@ -0,0 +1,107 @@
+#include "ladder.h"
+#include <cassert>
+
+void error(string word1, string word2, string msg) {
+    std::cerr << "Error: " << word1 << " -> " << word2 << ": " << msg << std::endl;
+}
+
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
+    int lenStr1 = str1.size();
+    int lenStr2 = str2.size();
+
+    if (abs(lenStr1 - lenStr2) > d)
+        return false;
+
+    int countDiff = 0;
+    int i = 0;
+    int j = 0;
+
+    while (i < lenStr1 && j < lenStr2) {
+        if (str1[i] != str2[j]) {
+            ++countDiff;
+            if (countDiff > d)
+                return false;
+
+            if (lenStr1 > lenStr2)
+                ++i;
+            else if (lenStr1 < lenStr2)
+                ++j;
+            else
+                ++i;
+                ++j;
+        } else {
+            ++i;
+            ++j;
+        }
+    }
+
+    if (i < lenStr1 || j < lenStr2)
+        ++countDiff;
+
+    return countDiff == d;
+}
+
+bool is_adjacent(const string& word1, const string& word2) {
+    return edit_distance_within(word1, word2, 1);
+}
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+    
+    set<string> visited;
+    visited.insert(begin_word);
+
+    while (!ladder_queue.empty()) {
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+
+        string last_word = ladder.back();
+
+        for (const string& word : word_list) {
+            if (is_adjacent(last_word, word)) {
+                if (visited.find(word) == visited.end()) {
+                    visited.insert(word);
+                    vector<string> new_ladder = ladder;
+                    new_ladder.push_back(word);
+                    if (word == end_word)
+                        return new_ladder;
+                    ladder_queue.push(new_ladder);
+                }
+            }
+        }
+    }
+    return {};
+}
+
+void load_words(set<string> & word_list, const string& file_name) {
+    ifstream inFile(file_name);
+    string word;
+    while (inFile >> word)
+        word_list.insert(word);
+    inFile.close();
+}
+
+void print_word_ladder(const vector<string>& ladder) {
+    if (ladder.empty())
+        std::cout << "No Word Ladder.";
+
+    for (const string& word : ladder) {
+        std::cout << word << " ";
+    }
+    std::cout << std::endl;
+}
+
+#define my_assert(condition) { cout << ((condition) ? " Passed" : " Failed") << endl; }
+
+void verify_word_ladder() {
+    set<string> word_list;
+
+    load_words(word_list, "words.txt");
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+}
